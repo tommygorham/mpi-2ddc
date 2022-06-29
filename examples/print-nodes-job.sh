87d654d953d2b175e664dc05a9130e18d91177b3
Tommy Gorham <38857089+tommygorham@users.noreply.github.com>
2022-06-29 15:05:59 -0400

    add slurm script for print-nodes.cpp

---
+++ examples/print-nodes-job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J run_print_nodes             # .exe from compiling print-nodes.cpp 
+#SBATCH -N 2                           # Execute on 2 nodes in the cluster (you may have more available, but start here if new to mpi)  
+#SBATCH -n 4                           # Execute the code with 4 MPI processes 
+#SBATCH -t 0:05:00                     # Max time allowed to run, it will be much faster than this 
+#SBATCH -o 01-multinode-job.out        # .out file generated from running the job with SLURM to see the nodes/ distributed mpi processes  
+
+# Run the job (assumes the .sh is submitted from the same directory)
+mpirun ./run_print_nodes
